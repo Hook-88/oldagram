fc266f054238651a47bcde3cda28290a2bf71789
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-04-28 16:48:49 -0500

    Add logic to display posts

--- script.js
+++ script.js
@@ -27,3 +27,44 @@ const posts = [
       likes: 152
   }
 ]
+
+const mainContainer = document.getElementById('main-container')
+
+function renderPosts() {
+  let postContainer = document.createElement("div")
+  for (const poster of posts) {
+    let post = renderPost(poster) 
+    postContainer.appendChild(post)
+  }
+  mainContainer.appendChild(postContainer)
+}
+
+
+function renderPost(poster) {
+  const post = document.createElement("section")
+  post.classList.add("post")
+  post.innerHTML = `
+                    <header class="side-padding white-bg">
+                      <img src="${poster.avatar}" alt="avatar of ${poster.name}" class="avatar-img">
+                      <div class="poster-info">
+                        <p class="bold-text">${poster.name}</p>
+                        <p>${poster.location}</p>
+                      </div>
+                    </header>
+                    <main class="post-image-${poster.username}"></main>
+
+                    <footer class="side-padding white-bg">
+                      <div class="icons-container">
+                        <img src="./images/icon-heart.png" alt="Icon for like">
+                        <img src="./images/icon-comment.png" alt="Icon for comment">
+                        <img src="./images/icon-dm.png" alt="Icon for direct mail">
+                      </div>
+                      <p class="bold-text">${poster.likes} likes</p>   
+                      <p><span class="bold-text">${poster.username}</span> ${poster.comment}</p>
+                    </footer>
+                  `
+  return post
+}
+
+
+renderPosts()
